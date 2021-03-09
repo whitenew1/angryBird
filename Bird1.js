3bf92f63b2fec45fd40e1317a4232c4aec081aaa
whitenew1 <75748757+whitenew1@users.noreply.github.com>
2021-03-09 12:39:16 +0530

    Add files via upload

---
+++ Bird1.js
@@ -0,0 +1,18 @@
+class Bird1 extends BaseClass{
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("sprites/bird.png");
+    }
+    display(){
+      this.body.position.x = mouseX;
+      this.body.position.y = mouseY;
+      super.display();
+    }
+  };
+  
+
+
+
+  /*
+
+  */
